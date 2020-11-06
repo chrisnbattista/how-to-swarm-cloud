4bbc70a8ad6aeb84f3787ddfdb2f29ce31b8161e
Chris Battista <c.battista@outlook.com>
2020-11-05 15:52:11 -1000

    trivial case with null forces working

--- particle_sim/experiments.py
+++ particle_sim/experiments.py
@@ -13,7 +13,7 @@ def set_up_experiment(n_particles, radius, particle_props=[]):
     # b_1 ... b_n are position expressed in basis vectors
 
     # Set state machine standard vars
-    world_state = pd.Dataframe(
+    world_state = pd.DataFrame(
         {
             'id':   [],
             'b_1':    [],
@@ -27,19 +27,22 @@ def set_up_experiment(n_particles, radius, particle_props=[]):
         world_state[prop] = []
 
     # create a random distribution of particles
-    for i in range(len(n_particles)):
-        theta = random.random() * 2 * math.PI
+    for i in range(n_particles):
+        theta = random.random() * 2 * math.pi
         r = random.random() * radius
-        world_state.append(
+        world_state = world_state.append(
             {
-                'id':   i,
-                'b_1':    r * math.cos(theta),
-                'b_2':    r * math.sin(theta),
-                't':    0
-            }
+                'id':       i,
+                'b_1':      r * math.cos(theta),
+                'b_2':      r * math.sin(theta),
+                't':        0
+            },
+            ignore_index=True
         )
+    
+    world_state['id'] = world_state['id'].astype(int)
 
-    return world_state
+    return world_state.set_index('id')
 
 def advance_timestep(world, timestep, integrator, pairwise_forces=[]):
     '''
@@ -52,12 +55,15 @@ def advance_timestep(world, timestep, integrator, pairwise_forces=[]):
 
     # Initialize matrix to hold forces keyed to id
     force_matrix = pd.DataFrame(
-                        world.id.append( np.zeros((world.size[0], 2)) ),
-                        columns=['id', 'b_1', 'b_2']
+                        index=world.index
                         )
+    force_matrix[['b_1', 'b_2']] = np.zeros( (len(world.index), 2) )
 
     for force in pairwise_forces:
         force_matrix += force(world)
+    
+    ## Advance the timestep itself
+    world['t'] += timestep
     
     ## Integrate forces over timestep
     return integrator(world, force_matrix, timestep)
