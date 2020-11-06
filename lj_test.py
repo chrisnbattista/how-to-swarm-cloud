2e267134dedef65cfcbc104b03fcb4eebda51f65
Chris Battista <c.battista@outlook.com>
2020-11-06 07:22:09 -1000

    more progress and visualization upgrades

--- lj_test.py
+++ lj_test.py
@@ -3,23 +3,36 @@ from particle_sim import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import pandas as pd
+
 plt.ion()
 plt.show()
+sns.set_theme()
 
-world = experiments.set_up_experiment(10, 10)
+world = experiments.set_up_experiment(radius=5, n_particles=15)
+history = []
+long_world_history = world
 
 for i in range(1000):
+
     if i % 10 == 0:
-        ##plt.clf()
-        sns.scatterplot(
-            x=world['b_1'],
-            y=world['b_2']
+        plt.clf()
+        p = sns.scatterplot(
+            x='b_1',
+            y='b_2',
+            hue='t',
+            data=long_world_history
         )
         plt.title(f"LJ Sim Timestep {i}")
+        p.legend_.remove()
         plt.pause(0.001)
+    
+    history.append(world)
+    long_world_history = pd.concat([long_world_history, world], axis=0, ignore_index=True)
+
     world = experiments.advance_timestep(
         world,
         0.1,
         integrators.integrate_rect_world,
-        [lambda x: potentials.pairwise_world_lennard_jones_potential(x, 10, 1)]
+        [lambda x: potentials.pairwise_world_lennard_jones_potential(x, 25, 1)]
     )
