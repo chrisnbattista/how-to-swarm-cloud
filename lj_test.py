31c4a919711aa9c7108a43d3bbb4553047e92025
Chris Battista <c.battista@outlook.com>
2020-11-06 08:10:21 -1000

    profiling to try and speed up computation

--- lj_test.py
+++ lj_test.py
@@ -9,30 +9,36 @@ plt.ion()
 plt.show()
 sns.set_theme()
 
-world = experiments.set_up_experiment(radius=5, n_particles=15)
-history = []
-long_world_history = world
+def run_sim():
 
-for i in range(1000):
+    world = experiments.set_up_experiment(radius=5, n_particles=15)
+    history = []
+    long_world_history = world
 
-    if i % 10 == 0:
-        plt.clf()
-        p = sns.scatterplot(
-            x='b_1',
-            y='b_2',
-            hue='t',
-            data=long_world_history
+    for i in range(1000):
+
+        if i % 10 == 0:
+            plt.clf()
+            p = sns.scatterplot(
+                x='b_1',
+                y='b_2',
+                hue='t',
+                data=long_world_history
+            )
+            plt.title(f"LJ Sim Timestep {i}")
+            p.legend_.remove()
+            plt.pause(0.001)
+        
+        history.append(world)
+        long_world_history = pd.concat([long_world_history, world], axis=0, ignore_index=True)
+
+        world = experiments.advance_timestep(
+            world,
+            0.1,
+            integrators.integrate_rect_world,
+            [lambda x: potentials.pairwise_world_lennard_jones_potential(x, 25, 1)]
         )
-        plt.title(f"LJ Sim Timestep {i}")
-        p.legend_.remove()
-        plt.pause(0.001)
-    
-    history.append(world)
-    long_world_history = pd.concat([long_world_history, world], axis=0, ignore_index=True)
+        print(world.info())
 
-    world = experiments.advance_timestep(
-        world,
-        0.1,
-        integrators.integrate_rect_world,
-        [lambda x: potentials.pairwise_world_lennard_jones_potential(x, 25, 1)]
-    )
+if __name__ == '__main__':
+    run_sim()
