c5caf849b86f5dfc162dd0353d9242ec024660dc
Chris Battista <c.battista@outlook.com>
2020-11-11 13:02:43 -1000

    Developed Jupyter notebook to house sim

--- lj_test.py
+++ lj_test.py
@@ -5,12 +5,39 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 import datetime as dt
+import time
 
-plt.ion()
-plt.show()
-sns.set_theme()
+def setup_plots():
+    plt.ion()
+    plt.show()
+    sns.set_theme()
 
-def run_sim():
+def lj_desktop_data_viz(world, long_world_history, i):
+    if i % 100 == 0:
+        plt.clf()
+        p = sns.scatterplot(
+            x='b_1',
+            y='b_2',
+            s=5,
+            hue='t',
+            data=long_world_history
+        )
+        p.legend_.remove()
+        p2 = sns.scatterplot(
+            x='b_1',
+            y='b_2',
+            color='k',
+            data=world
+        )
+        p2.legend_.remove()
+        plt.title(f"LJ Sim Timestep {i}")
+        plt.pause(0.00001)
+
+def run_sim(data_viz=lj_desktop_data_viz):
+
+    ## Record keeping
+    start_time = time.time()
+    last_loop_time = start_time
 
     ## Parameters
     timestep = 0.01
@@ -27,42 +54,29 @@ def run_sim():
     try:
         for i in range(100000):
 
-            # Data viz
-            if i % 100 == 0:
-                plt.clf()
-                p = sns.scatterplot(
-                    x='b_1',
-                    y='b_2',
-                    s=5,
-                    hue='t',
-                    data=long_world_history
-                )
-                p.legend_.remove()
-                p2 = sns.scatterplot(
-                    x='b_1',
-                    y='b_2',
-                    color='k',
-                    data=world
-                )
-                p2.legend_.remove()
-                plt.title(f"LJ Sim Timestep {i}")
-                plt.pause(0.001)
+            ## Record keeping
+            loop_duration = time.time() - last_loop_time
+            last_loop_time += loop_duration
+            print(loop_duration)
+
+            ## Data viz
+            data_viz(world, long_world_history, i)
             
-            # Trajectory recording
+            ## Trajectory recording
             long_world_history = pd.concat([long_world_history, world.reset_index()], axis=0, ignore_index=True)
 
-            # Sim step
+            ## Sim step
             world = experiments.advance_timestep(
                 world,
                 timestep,
                 integrators.integrate_rect_world,
                 [
-                    lambda x: forces.pairwise_world_lennard_jones_potential(x, 1, 1),
+                    lambda x: forces.pairwise_world_lennard_jones_potential(x, epsilon=1, omega=1),
                     lambda x: forces.viscous_damping_force(x, 0.005)
                 ]
             )
             
-            # BCs (periodic square)
+            ## BCs (periodic square)
             world['b_1'] = world['b_1'] % size
             world['b_2'] = world['b_2'] % size
 
@@ -73,4 +87,5 @@ def run_sim():
     long_world_history.to_csv("LJ Sim Run_" + str(dt.datetime.now()) + ".csv")
 
 if __name__ == '__main__':
+    setup_plots()
     run_sim()
