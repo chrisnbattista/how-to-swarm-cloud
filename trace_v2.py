08ee70e8dbcfa4a5869beb716d3ad455feea8b53
Chris Battista <c.battista@outlook.com>
2021-04-21 14:46:30 -0400

    better trace

---
+++ trace_v2.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import random, glob
+import pandas as pd
+
+## Select dataset
+data_choice = input("which dataset>").strip()
+
+## Get data
+path = random.choice(glob.glob(f'./data/two_particle/{data_choice}/*.csv'))
+print('Loading random data file from dataset...')
+data = pd.read_csv(
+                path,
+                delimiter=',', 
+                index_col=False
+            )
+
+## Plot trace
+fig = plt.figure()
+plt.scatter(
+                    x=data['b_1'],
+                    y=data['b_2'],
+                    s=1
+                )
+plt.title('Parametric plot of x(t)')
+fig.canvas.set_window_title('Trace')
+plt.xlabel('x_1')
+plt.ylabel('x_2')
+plt.show()
